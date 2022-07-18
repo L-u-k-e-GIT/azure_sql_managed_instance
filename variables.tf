b5587083ad69b4ad8f85fe880cf70b4a48dc74e8
Luke <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-07-18 15:33:49 +0200

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,59 @@
+
+
+
+
+########### MODULE VARIABLES
+
+variable "MD_LOCATION" {
+  description = "Region where deploy disk"
+  type        = string
+  default     = ""
+}
+
+variable "MD_RG_NAME" {
+  description = "Resource Group where deploy disk"
+  type        = string
+  default     = ""
+}
+
+variable "MD_VM_ENV" {
+  description = "Enviroment of the resource"
+  type        = string
+  default     = ""
+}
+
+
+###########NAMING CONVENTION PREFIX
+
+variable "MD_PROJECT_NAME" {
+  description = "Project Name Prefix"
+  type        = string
+  default     = ""
+}
+
+variable "MD_SUBSCRIPTION_PREFIX" {
+  description = "Subscription Prefix"
+  type        = string
+  default     = ""
+}
+
+variable "MD_REGION_PREFIX" {
+  description = "Region Prefix"
+  type        = string
+  default     = ""
+}
+
+variable "MD_SQL_MANAGED_PREFIX" {
+  description = "SQL MANAGED INSTANCE Prefix"
+  type        = string
+  default     = ""
+}
+
+variable "MD_SQL_ADMIN" {}
+variable "MD_SQL_PWD" {}
+variable "MD_SQL_LICENSE_TYPE" {}
+variable "MD_SUBNET_ID" {}
+variable "MD_SQL_SKU" {}
+variable "MD_SQL_CORE" {}
+variable "MD_SQL_DISK_SIZE" {}
+
